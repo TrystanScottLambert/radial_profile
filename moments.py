376ed60745617532b2290e88455ee1f145bfc29c
Trystan Lambert <trystanscottlambert@gmail.com>
2023-05-05 13:24:33 -0400

    renaming

--- moments.py
+++ moments.py
@@ -12,4 +12,12 @@ class IntegratedMoment:
     """The moment 0 map of a radio observation."""
     def __init__(self, cube_name: str) -> None:
         """Initializing the moment map by reading in the file name."""
-        
+        self.hdul = fits.open(cube_name)
+        self.bmaj = self.hdul[0].header['BMAJ']
+        self.bmin = self.hdul[0].header['BMIN']
+        self.bpa = self.hdul[0].header['BPA']
+    
+
+if __name__ == '__main__':
+    infile = '/home/tlambert/Desktop/HZ7/Hz7_ISM/data/HZ7_mom_mask2.integrated.fits'
+    hz7_mom0 = IntegratedMoment(infile)
